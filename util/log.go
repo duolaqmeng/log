d96465da362428ea4764a32e38f015690786c9eb
hezhaohui1 <381162736@qq.com>
2020-07-08 17:40:24 +0800

    v2

--- util/log.go
+++ util/log.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	Debug  = iota
+	Debug = iota
 	Trace
 	Info
 	Warn
@@ -16,14 +16,14 @@ const (
 )
 
 type Logger struct {
-	Level int
+	Level       int
 	logfilePath string
 	logfileName string
-	file *os.File
+	file        *os.File
 }
 
-func NewLogger(level int,logfilePath, logfileName string) *Logger{
-	f1:=&Logger{
+func NewLogger(level int, logfilePath, logfileName string) *Logger {
+	f1 := &Logger{
 		Level:       level,
 		logfileName: logfileName,
 		logfilePath: logfileName,
@@ -32,51 +32,52 @@ func NewLogger(level int,logfilePath, logfileName string) *Logger{
 	return f1
 }
 
-
-func(l *Logger)initLogger(){
-	filepath:=fmt.Sprintf("%s%s",l.logfilePath,l.logfileName)
-	file ,err:=os.OpenFile(filepath,os.O_CREATE|os.O_WRONLY|os.O_APPEND,0655)
-	if err!=nil{
-		panic("open file "+filepath+" error")
+func (l *Logger) initLogger() {
+	filepath := fmt.Sprintf("%s%s", l.logfilePath, l.logfileName)
+	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0655)
+	if err != nil {
+		panic("open file " + filepath + " error")
 	}
-	l.file=file
+	l.file = file
 }
 
-func (l *Logger)Debug(format string, a ...interface{}){
-	if  l.Level>Debug{
+func (l *Logger) Debug(format string, a ...interface{}) {
+	if l.Level > Debug {
 		return
 	}
-	l.Common(format ,a...)
+	l.Common(format, a...)
 }
-func (l *Logger)Trace(format string, a ...interface{}){
-	if  l.Level>Trace{
+func (l *Logger) Info(format string, a ...interface{}) {
+	if l.Level > Info {
 		return
 	}
-	l.Common(format,a...)
+	l.Common(format, a...)
 }
-func (l *Logger)Warn(format string, a ...interface{}){
-	if  l.Level>Warn{
+func (l *Logger) Trace(format string, a ...interface{}) {
+	if l.Level > Trace {
 		return
 	}
-	l.Common(format,a...)
+	l.Common(format, a...)
+}
+func (l *Logger) Warn(format string, a ...interface{}) {
+	if l.Level > Warn {
+		return
+	}
+	l.Common(format, a...)
 }
 
-func (l *Logger)Error(format string, a ...interface{}){
-	if  l.Level>Error{
+func (l *Logger) Error(format string, a ...interface{}) {
+	if l.Level > Error {
 		return
 	}
-	l.Common(format,a...)
+	l.Common(format, a...)
 }
 
-func (l *Logger)Common(format string ,a... interface{}){
-	timeStr:=time.Now().Format("[2006-01-02 15:04:05.000]")
+func (l *Logger) Common(format string, a ...interface{}) {
+	timeStr := time.Now().Format("[2006-01-02 15:04:05.000]")
 	funcname, file, line := getCallerInfo()
-	format=fmt.Sprintf("[%s][-%s-] [%s %s:%d] %s",
-		timeStr,getLevelStr(l.Level),file,funcname,line,format)
-	fmt.Fprintf(l.file,format,a...)
+	format = fmt.Sprintf("[%s][-%s-] [%s %s:%d] %s",
+		timeStr, getLevelStr(l.Level), file, funcname, line, format)
+	fmt.Fprintf(l.file, format, a...)
 	fmt.Fprintln(l.file)
 }
-
-
-
-
