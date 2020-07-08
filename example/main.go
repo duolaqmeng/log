1549bb0feda21c7902200729076fb4d62ddc7eb4
hezhaohui1 <381162736@qq.com>
2020-07-08 17:38:35 +0800

    v2

--- example/main.go
+++ example/main.go
@@ -6,6 +6,5 @@ func main() {
 	f1 := util.NewLogger(util.Info, "./", "xx")
 	a := "test"
 	f1.Info("this is %s hello1111111111", a)
-	f1.Info("this is %s hello1111111111", a)
-	f1.Info("this is %s hello1111111111", a)
+
 }
