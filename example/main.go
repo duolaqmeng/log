ac48e535f2fbe169592dedf566e50578941406bf
hezhaohui1 <381162736@qq.com>
2020-07-08 17:26:21 +0800

    V2

--- example/main.go
+++ example/main.go
@@ -3,8 +3,9 @@ package main
 import "log/util"
 
 func main() {
-	f1:=util.NewLogger(util.Info,"./","xx")
-	a:="test"
-	f1.Info("this is %s hello1111111111",a)
-	f1.Info("this is %s hello1111111111",a)
+	f1 := util.NewLogger(util.Info, "./", "xx")
+	a := "test"
+	f1.Info("this is %s hello1111111111", a)
+	f1.Info("this is %s hello1111111111", a)
+	f1.Info("this is %s hello1111111111", a)
 }
